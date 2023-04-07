d6833fc5754f18eaf818547444ac80f5985f4668
Hamza <alfadelhamza@gmail.com>
2023-04-07 01:12:49 +0100

    add app

---
+++ experiments/text-recognition/app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from mangum import Mangum
+import asyncio
+
+
+app = FastAPI()
+
+
+
+@app.get("/")
+async def get():
+    return {
+        'message': 'Hello World! Welcome to the (experimental) image recognition API'
+    }
+
+
+handler = Mangum(app)
+
+
+
+
+
