b7df2463558a2d0ed0836589dacce5ce9ae63b08
Hamza <alfadelhamza@gmail.com>
2023-04-09 16:19:11 +0100

    edit lesson 2

--- fastai/lesson_2.py
+++ fastai/lesson_2.py
@@ -1,129 +1,86 @@
-"""
-In this lesson, I deploy a simple image classifier to the web using fastai and streamlit
-Note - Code below not finished yet
-"""
+# Re-implementation of the lesson 1 notebook in fastai v1 in a python script
+# that I can run from the command line. Small edits.
+# Author: Jeremy Howard
 
-import streamlit as st
-from fastai.vision.all import load_learner, PILImage
-from PIL import Image
-import os
 from duckduckgo_search import ddg_images
 from fastdownload import download_url
 from fastai.vision.all import Image
 from fastai.vision.all import *
-from fastai.losses import CrossEntropyLossFlat
-
-import numpy as np  # linear algebra
-import struct
-from array import array
-from os.path import join
-import random
-import matplotlib.pyplot as plt
-import os
+from fastcore.all import *
+# import fire
+from time import sleep
 
 
-input_path = os.getcwd() + "/datasets/MNIST"
-training_images_filepath = join(
-    input_path, "train-images-idx3-ubyte/train-images-idx3-ubyte"
-)
-training_labels_filepath = join(
-    input_path, "train-labels-idx1-ubyte/train-labels-idx1-ubyte"
-)
-test_images_filepath = join(input_path, "t10k-images-idx3-ubyte/t10k-images-idx3-ubyte")
-test_labels_filepath = join(input_path, "t10k-labels-idx1-ubyte/t10k-labels-idx1-ubyte")
-
-class MnistDataloader(object):
+def search_images(term, max_images=30):
     """
-    MNIST data loader class
+    Searches duckduckgo for images of a given term and returns a list of urls
     """
+    print(f"Searching for '{term}...'")
+    return [item["image"] for item in ddg_images(term, max_results=max_images)]
 
-    def __init__(
-        self,
-        training_images_filepath,
-        training_labels_filepath,
-        test_images_filepath,
-        test_labels_filepath,
-    ):
-        self.training_images_filepath = training_images_filepath
-        self.training_labels_filepath = training_labels_filepath
-        self.test_images_filepath = test_images_filepath
-        self.test_labels_filepath = test_labels_filepath
 
-    def read_images_labels(self, images_filepath, labels_filepath):
-        """
-        Read MNIST images and labels from specified filepaths
-        images_filepath: path to image data file
-        labels_filepath: path to label data file
-        """
-        labels = []
-        with open(labels_filepath, "rb") as file:
-            magic, size = struct.unpack(">II", file.read(8))
-            if magic != 2049:
-                raise ValueError(
-                    "Magic number mismatch, expected 2049, got {}".format(magic)
-                )
-            labels = array("B", file.read())
+def main(
+    search_terms: tuple,
+    max_images: int = 10,
+    generate_images: bool = False,
+    use_existing_model: bool = False,
+):
+    label_a, label_b = search_terms
+    current_dir = Path.cwd()
+    path = current_dir / f"{label_a}_or_{label_b}"
+    print(f"Path: {path}")
+    # path = Path(f"{label_a}_or_{label_b}")
 
-        with open(images_filepath, "rb") as file:
-            magic, size, rows, cols = struct.unpack(">IIII", file.read(16))
-            if magic != 2051:
-                raise ValueError(
-                    "Magic number mismatch, expected 2051, got {}".format(magic)
-                )
-            image_data = array("B", file.read())
-        images = []
-        for i in range(size):
-            images.append([0] * rows * cols)
-        for i in range(size):
-            img = np.array(image_data[i * rows * cols : (i + 1) * rows * cols])
-            img = img.reshape(28, 28)
-            images[i][:] = img
+    if generate_images:
+        generate_labelled_dataset(search_terms, path, max_images)
 
-        return images, labels
+    verify(path)
+    dls = get_dls(path)
 
-    def load_data(self):
-        """
-        Load MNIST data
-        """
-        x_train, y_train = self.read_images_labels(
-            self.training_images_filepath, self.training_labels_filepath
-        )
-        x_test, y_test = self.read_images_labels(
-            self.test_images_filepath, self.test_labels_filepath
-        )
-        return (x_train, y_train), (x_test, y_test)
-    
-    def transform_data(self, data: tuple):
-        """
-        Transform MNIST data into pytorch tensors
-        data: a tuple of (x, y) where x is a list of images and y is a list of labels
-        """
-        x, y = data
-        x = np.array(x)[:, np.newaxis, :, :] / 255.0  # Convert list of ndarrays to a single ndarray and normalize
-        x = np.repeat(x, 3, axis=1)        
-        x = torch.tensor(x, dtype=torch.float32)     # Convert the ndarray to a tensor
-        y = torch.tensor(y, dtype=torch.long)        # Convert the labels to a tensor
-        return list(zip(x, y))
+    if use_existing_model:
+        model = load_learner("models/fine_tuned_resnet18.pkl")
+    else:
+        model = learn(dls)
 
-def show_images(images, title_texts):
+    predict(model, label_a)
+
+
+def generate_labelled_dataset(search_terms: tuple, path: Path, max_images: int = 10):
     """
-    Show a list of images in a grid
-    images: a list of images
-    title_texts: a list of title texts
+    Simple wrapper around search_images and download_images
     """
-    cols = 5
-    rows = int(len(images) / cols) + 1
-    plt.figure(figsize=(30, 20))
-    index = 1
-    for x in zip(images, title_texts):
-        image = x[0]
-        title_text = x[1]
-        plt.subplot(rows, cols, index)
-        plt.imshow(image, cmap=plt.cm.gray)
-        if title_text != "":
-            plt.title(title_text, fontsize=15)
-        index += 1
-    plt.show()
+    for search_term in search_terms:
+        dest = path / search_term
+        dest.mkdir(exist_ok=True, parents=True)
+        download_images(dest, urls=search_images(f"{search_term} photo"))
+        sleep(0.5)  # Pause between searches to avoid over-loading server
+        download_images(dest, urls=search_images(f"{search_term} sun photo"))
+        sleep(0.5)
+        download_images(dest, urls=search_images(f"{search_term} shade photo"))
+        sleep(0.5)
+        resize_images(path / search_term, max_size=400, dest=path / search_term)
+
+
+def verify(path: str) -> None:
+    """
+    Verify that the images in the given path are valid.
+    Note: Functional programming approach (new to me)
+    """
+    failed = verify_images(get_image_files(path))
+    failed.map(Path.unlink)
+    print(f"Removed {len(failed)} images that failed to open")
+
+
+def learn(dls, save: bool = True):
+    """
+    Returns the pre-trained resnet18 fine-tuned on the given dataset
+    """
+    model = vision_learner(dls, resnet18, metrics=error_rate)
+    model.fine_tune(3)
+    if save:
+        model.export("fine_tuned_model.pkl")
+    print(f"Finished fine-tuning. Error rate: {model.validate()}")
+    return model
 
 
 def get_dls(path, bs=32, size=192):
@@ -137,46 +94,34 @@ def get_dls(path, bs=32, size=192):
         get_y=parent_label,
         item_tfms=[Resize(size, method="squish")],
     ).dataloaders(path, bs=bs)
-    
-    
-def learn(dls, model_name: str, save: bool = True):
-    """
-    Returns the pre-trained resnet18 fine-tuned on the given dataset
-    """
-    model = vision_learner(dls, resnet18, metrics=error_rate, loss_func=CrossEntropyLossFlat())
-    model.fine_tune(1)
-    # if save:
-    #     model.export(f"model/{model_name}.pkl")
-    if save:
-        torch.save(model.model.state_dict(), f"models/{model_name}.pth")
-    print(f"Finished fine-tuning. Error rate: {model.validate()}")
-    return model
 
-def get_dls(train_data, test_data, bs=64, n_out=10):
-    dls = DataLoaders.from_dsets(train_data, test_data, bs=bs, shuffle=True, drop_last=True)
-    dls.c = n_out # set the number of output classes (10 for MNIST)
-    return dls
 
-def main():
+def predict(model: object, search_term: str):
     """
-    1. Instantiate the MNIST data loader
-    2. Load the MNIST dataset
-    3. Create a DataLoaders object for the MNIST dataset
-    4. Fine-tune a pre-trained resnet18 model on the MNIST dataset
-    5. Save the fine-tuned model
+    Predicts the label of the given image
+    Returns the label and the probability of that label
     """
-    mnist_dataloader = MnistDataloader(
-    training_images_filepath,
-    training_labels_filepath,
-    test_images_filepath,
-    test_labels_filepath,
-    )
-    (x_train, y_train), (x_test, y_test) = mnist_dataloader.load_data()
-    train_data = mnist_dataloader.transform_data((x_train, y_train))
-    test_data = mnist_dataloader.transform_data((x_test, y_test))
+    dest = f"{search_term}s.jpg"
+    urls = search_images(f"{search_term} photos", max_images=1)
+    download_url(urls[0], dest, show_progress=False)
+    name, _, probs = model.predict(PILImage.create(dest))
+
+    print(f"This is a: {name}.")
+    print(f"Probability it's a {name}: {probs[0]:.4f}")
     
-    dls = get_dls(train_data, test_data)
-    learn(dls, "resnet18-fine-tuned-on-mnist")
+def predict_from_path(model: object, image: str):
+    """
+    Predicts the label of the given image
+    Returns the label and the probability of that label
+    """
+    dest = Path.cwd() / "images" / f"{image}"
+    # pick random image from the given path
+    dest = random.choice([x for x in dest.iterdir() if x.is_file()])
+    print(dest)
+    name, _, probs = model.predict(PILImage.create(dest))
+    print(f"This is a: {name}.")
+    print(f"Probability it's a {name}: {probs[0]:.4f}")
     
-if __name__ == "__main__":
-    main()
+# main(('bike', 'tiger'), max_images=30, generate_images=True, use_existing_model = False)
+
+predict(load_learner("fine_tuned_model.pkl"), 'tiger')
