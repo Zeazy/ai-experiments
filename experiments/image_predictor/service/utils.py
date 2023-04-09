35fb591acf051ecc09e5aa557f29bf44e8b0775c
Hamza <alfadelhamza@gmail.com>
2023-04-09 17:10:38 +0100

    formatting

--- experiments/image_predictor/service/utils.py
+++ experiments/image_predictor/service/utils.py
@@ -1,16 +1,23 @@
 from service.constants import CUSTOM_CSS
 import streamlit as st
 
+
 def inject_custom_css(css: str = CUSTOM_CSS):
-    st.markdown(f"""
+    st.markdown(
+        f"""
     <style>
         {css}
     </style>
-    """, unsafe_allow_html=True)
+    """,
+        unsafe_allow_html=True,
+    )
+
 
 def render_header():
     st.markdown('<div class="main">', unsafe_allow_html=True)
     st.title("Rhino Guessing Game 🦏")
-    st.subheader("""Generate a random picture of a rhino and see if you can guess the species 
+    st.subheader(
+        """Generate a random picture of a rhino and see if you can guess the species 
                 \n Species include: Black, White, Javan and Sumatran.
-                \n This is a simple image classification app - the model was trained on a small dataset of rhino images.""")  
+                \n This is a simple image classification app - the model was trained on a small dataset of rhino images."""
+    )
